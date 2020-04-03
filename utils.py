0f27f63cd5269dd23d925f0521562ac3f20c2f97
Mengzhou Xia <296337231@qq.com>
2020-04-02 22:00:25 -0400

    modified utils.

--- utils.py
+++ utils.py
@@ -1,17 +1,25 @@
 import pickle as pkl
 import numpy as np
-from copy import deepcopy
-import warnings
-from gp import ExactGPModel
 import lang2vec.lang2vec as l2v
 import glob
-import pandas as pd
 from logging import getLogger
+from task_feats import task_eval_columns
+import pandas as pd
+import os
+import sys
 
 logger = getLogger()
 
-def p2f(x):
-    return float(x.strip('%'))/100
+def output_l2v_features(langs, task):
+    feats = ["syntax_average", "phonology_average", "inventory_average", "geo", "fam"]
+    d = {}
+    for langss in langs:
+        for lang in langss:
+            if lang not in d:
+                features = l2v.get_features(lang, "+".join(feats))
+                d[lang] = features
+    with open(os.path.join(sys.argv[1], 'data', '{}.vec.pkl'.format(task), 'wb')) as handle:
+        pkl.dump(d, handle, protocol=pkl.HIGHEST_PROTOCOL)
 
 
 def convert_label(df):
@@ -24,44 +32,6 @@ def load_pkl_file(file):
 def save_pkl_file(ob, file, paras=None):
     ob["paras"] = paras
     pkl.dump(ob, open(file, "wb"))
-
-
-def standardize_feats_df(train_feats, test_feats, return_mean_var=False):
-    columns = train_feats.columns
-    train_feats = deepcopy(train_feats)
-    test_feats = deepcopy(test_feats)
-    mns = None; sstd = None
-    with warnings.catch_warnings():
-        warnings.filterwarnings('error')
-        c = None
-        try:
-            for c in columns:
-                if list(set(train_feats[c].values)) == [0, 1]: # a stupid way to identify dummy variables
-                    pass
-                else:
-                    values, mns, sstd = zscore(train_feats[c].values)
-                    train_feats[c] = values
-                    test_feats[c] = zscore(test_feats[c].values, mns=mns, sstd=sstd)[0]
-        except Warning:
-            logger.warning("Standardization goes wrong for column {} ...".format(c))
-    if return_mean_var:
-        assert len(columns) == 1
-        return train_feats, test_feats, mns, sstd
-    else:
-        return train_feats, test_feats
-
-
-def zscore(a, axis=0, ddof=0, mns=None, sstd=None):
-    a = np.asanyarray(a)
-    if mns is None:
-        mns = a.mean(axis=axis)
-    if sstd is None:
-        sstd = a.std(axis=axis, ddof=ddof)
-    if axis and mns.ndim < a.ndim:
-        return ((a - np.expand_dims(mns, axis=axis)) /
-                np.expand_dims(sstd, axis=axis))
-    else:
-        return (a - mns) / sstd, mns, sstd
 
 
 def recover(mns, sstd, test_labels):
@@ -104,3 +74,29 @@ def log_args(args):
     for key in args_dict.keys():
         value = args_dict[key]
         logger.info("{}: {}".format(key, value))
+
+def to_csv():
+    lines = open("/home/mengzhox/scripts/nlppred/model_xgboost_ud.log", "r").readlines()
+    index = list(set([l.split()[0] for l in lines]))
+    columns = list(set([l.split()[2][:-1] for l in lines]))
+    df = pd.DataFrame(columns=[f"RMSE@{i}" for i in range(5)], index=index)
+    for line in lines:
+        i = line.split()[0]
+        c = line.split()[-2][:-1]
+        df.loc[i, c] = float(line.split()[-1])
+    lines = open("/home/mengzhox/scripts/nlppred/model_xgboost_ud_mean.log", "r").readlines()
+    for line in lines:
+        i = line.split()[0]
+        c = "RMSE@0"
+        df.loc[i, c] = float(line.split()[-2])
+    df.to_csv("/home/mengzhox/scripts/nlppred/model_xgboost_ud.csv")
+
+def correlation():
+    df = pd.read_csv("data/data_ud.csv")[task_eval_columns("ud")]
+
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', -1)
+    print(df.corr(method ='pearson'))
+
